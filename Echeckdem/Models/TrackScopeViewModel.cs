99e0c242a30ac61e381f661a02812cd2f2519e6f
Rohit Arora <Rohit.Arora@Comply4hr.com>
2025-03-05 11:46:31 +0530

    fe for track scope

---
+++ Echeckdem/Models/TrackScopeViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class TrackScopeViewModel
+{
+    public int WorkId { get; set; }
+    public string Task { get; set; }
+    public int Reminder { get; set; }
+    public int FirstAlert { get; set; }
+    public string ScopeName { get; set; }
+    public string StateName { get; set; }
+}
