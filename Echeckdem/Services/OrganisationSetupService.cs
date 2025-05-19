4d91ff3df0d022dcbfe55eedab7882dc2c72c9bd
sushant dixit <dixit.sushant6@gmail.com>
2025-05-19 14:18:14 +0530

    bulkuploadservice, organizationsetupservice

--- Echeckdem/Services/OrganisationSetupService.cs
+++ Echeckdem/Services/OrganisationSetupService.cs
@@ -292,13 +292,13 @@ namespace Echeckdem.Services
                         {
                             var lname = row.Cell(1).GetValue<string>()?.Trim();
                             //  Resolve lcode based on lname
-                            var site = boSites.FirstOrDefault(b => b.Lname.Equals(lname, StringComparison.OrdinalIgnoreCase));
+                            var site = boSites.FirstOrDefault(b => b.Lname.Trim().Equals(lname.Trim(), StringComparison.OrdinalIgnoreCase));
                             if (site == null)
-                                throw new InvalidOperationException($"Invalid Location Name (Lname): {lname} for BO site.");
+                                throw new InvalidOperationException($"Invalid Location Name (Lname): {lname}");
 
-                            var lcode = site.Lcode; // Use the resolved lcode for further processing
+                            var lcode = site.Lcode.Trim(); // Use the resolved lcode for further processing
 
-                            var resolvedLname = site.Lname; // Use the resolved lname for population
+                            var resolvedLname = site.Lname.Trim(); // Use the resolved lname for population
 
                             var projectAreaCell = row.Cell(7);
                             decimal? projectArea = null; // Use nullable decimal
@@ -387,7 +387,7 @@ namespace Echeckdem.Services
                                 OvalId = row.Cell(2).GetValue<string>().Trim(),
                                 ClientName = row.Cell(3).GetValue<string>().Trim(),
                                 GeneralContractor = row.Cell(4).GetValue<string>().Trim(),
-                                ProjectAddress = row.Cell(5).GetValue<string>().Trim(),
+                                ProjectAddress = row.Cell(5).GetValue<string>().Trim().Replace("\"",""),
                                 NatureofWork = row.Cell(6).GetValue<string>().Trim(),
                                 //ProjectArea = row.Cell(7).GetValue<decimal>(),
                                 //ProjectCostEst = row.Cell(8).GetValue<float?>(),
